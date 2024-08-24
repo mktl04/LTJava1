fcb5987d75cf7a5759f133f101c0c5a6c273719a
DELL <DELL@DESKTOP-48GHHT8>
2024-08-24 07:54:50 +0700

    PB1

--- src/main/java/BT03/SP.java
+++ src/main/java/BT03/SP.java
@@ -12,6 +12,9 @@ public class SP {
     private String TenSP;
     private double GiaSP;
     
+    public SP(){
+        
+    }
     public SP(String TenSP, double GiaSP){
         this.TenSP = TenSP;
         this.GiaSP = GiaSP;
@@ -21,6 +24,16 @@ public class SP {
     }
     public double getGiaSP(){
         return GiaSP;
+    }  
+    public void setTenSP(String TenSP){
+        this.TenSP = TenSP;
+    }
+    public void setGiaSP(double GiaSP){
+        this.GiaSP = GiaSP;
+    }
+    @Override
+    public String toString(){
+        return "SanPham" + "Ten: " + TenSP + "Gia: " + GiaSP;
     }
 }
 
