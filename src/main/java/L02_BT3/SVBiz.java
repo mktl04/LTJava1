247ad4e424c82f118c74edf46963dc73594fa832
DELL <DELL@DESKTOP-48GHHT8>
2024-08-26 07:44:17 +0700

    Update name

---
+++ src/main/java/L02_BT3/SVBiz.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package L02_BT3;
+
+/**
+ *
+ * @author DELL
+ */
+public class SVBiz extends SV {
+
+    public double diemMarketing;
+    public double diemSales;
+
+    public SVBiz(double diemMarketing, double diemSales, String hoTenSV, String nganh){
+        super(hoTenSV, nganh);
+        this.diemMarketing = diemMarketing;
+        this.diemSales = diemSales;
+    }
+
+    @Override
+    public double getDiem() {
+        return (2 * diemMarketing + diemSales) / 3;
+    }
+}
